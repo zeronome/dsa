c9048f5ac698f504131bf4ae13dcf9f408877e54
zeronome <bess.ufop@gmail.com>
2022-06-22 20:05:09 -0300

    Arrumando a disposição das pastas para separar por curso

---
+++ r_azure/Cap02/Exercicios-Cap02.R
@@ -0,0 +1,64 @@
+# Lista de Exercícios - Capítulo 2
+
+# Obs: Caso tenha problemas com a acentuação, consulte este link:
+# https://support.rstudio.com/hc/en-us/articles/200532197-Character-Encoding
+
+# Configurando o diretório de trabalho
+# Coloque entre aspas o diretório de trabalho que você está usando no seu computador
+# Não use diretórios com espaço no nome
+setwd("F:\OneDrive\Cursos\Avulsos\DSA-Cientista_dados\I_Azure_R\Cap02")
+getwd()
+
+# Exercício 1 - Crie um vetor com 30 números inteiros
+vector <- c(1:30)
+
+# Exercício 2 - Crie uma matriz com 4 linhas e 4 colunas preenchida com números inteiros
+matriz <- matrix(c(1:16), nr = 4)
+
+# Exercício 3 - Crie uma lista unindo o vetor e matriz criados anteriormente
+lista <- list(vetor = vector, matriz = matriz)
+
+
+# Exercício 4 - Usando a função read.table() leia o arquivo do link abaixo para uma dataframe
+# http://data.princeton.edu/wws509/datasets/effort.dat
+dataframe1 <- read.table("http://data.princeton.edu/wws509/datasets/effort.dat", header = TRUE)
+
+
+# Exercício 5 - Transforme o dataframe anterior, em um dataframe nomeado com os seguintes labels:
+# c("config", "esfc", "chang")
+dataframe2 <- dataframe1
+colnames(dataframe2, c("config", "esfc", "chang"))
+
+
+# Exercício 6 - Imprima na tela o dataframe iris, verifique quantas dimensões existem no dataframe iris e imprima um resumo do dataset
+print(iris)
+dim(iris)
+str(iris)
+summary(iris)
+
+# Exercício 7 - Crie um plot simples usando as duas primeiras colunas do dataframe iris
+plot(iris[[1]], iris[,2])
+plot(iris$Sepal.Length, iris$Sepal.Width, main = "Sepal Lenght vs Width", xlab = colnames(iris)[1], ylab = colnames(iris)[2])
+
+
+# Exercício 8 - Usando a função subset, crie um novo dataframe com o conjunto de dados do dataframe iris em que Sepal.Length > 7
+# Dica: consulte o help para aprender como usar a função subset()
+dataframe3 <- subset(iris, Sepal.Length > 7)
+
+# Exercícios 9 (Desafio) - Crie um dataframe que seja cópia do dataframe iris e usando a função slice(), divida o dataframe 
+# em um subset de 15 linhas
+# Dica 1: Você vai ter que instalar e carregar o pacote dplyr
+# Dica 2: Consulte o help para aprender como usar a função slice()
+install.packages("dplyr")
+library(dplyr)
+?slice
+sample <- slice_sample(iris, n = 15, weight_by = iris[[2]])
+# OBS: o [[]] permite passar a coluna como argumento de uma função em R
+
+# Exercícios 10 - Use a função filter no seu novo dataframe criado no item anterior e retorne apenas valores em que Sepal.Length > 6
+# Dica: Use o RSiteSearch para aprender como usar a função filter
+help.search('filter')
+??filter
+RSiteSearch('filter')
+?filter
+filtro <- filter(sample, Sepal.Length >6)
---
+++ r_azure/razure.md
@@ -0,0 +1,35 @@
+# Curso R com Azure 🐱‍👤
+
+#### by Data Science Academy 🐱‍🏍
+
+Este diretório objetiva armazenar todos os códigos do curso R com Azure da Formação Cientista de dados cursada por esta autora, bem como montar um portifólio com códigos originais desenvolvidos durante o curso.
+
+Os pontos importantes estão marcados nos códigos com IMP! para localização mais rápida. Também foi desenvolvido um condensador de todos os scripts do curso para facilitar buscas para ajuda em projetos futuros.
+
+
+#### Dicas Git e GitHub
+
+Para utilizar o Git com o Github e enviar os arquivos modificados para o repositório da nuvem utiliza-se o seguinte script:
+
+    echo "# cursos" >> README.md
+    git init
+    git add README.md
+    git commit -m "first commit"
+    git branch -M main
+    git remote add origin https://github.com/zeronome/cursos.git
+    git pull origin main
+    git push -u origin main
+    
+
+ 
+
+
+
+--- 
+title: "${1:Enter a Title}" 
+date: $$CURRENT\_YEAR-$$CURRENT_MONTH-$CURRENT_DATE 
+author: ${2:Enter your name} id: 
+$ZKN_ID --- 
+
+# ${1:Curso R com Azure} 
+$0
